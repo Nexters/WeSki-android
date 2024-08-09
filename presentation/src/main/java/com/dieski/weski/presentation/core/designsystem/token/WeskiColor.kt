8c3054b30c31c94595e2ce68c361a5cf59ac7860
JGeun <pjk8926@naver.com>
2024-08-09 14:47:34 +0900

    feat: Color 적용

---
+++ presentation/src/main/java/com/dieski/weski/presentation/core/designsystem/token/WeskiColor.kt
@@ -0,0 +1,33 @@
+package com.dieski.weski.presentation.core.designsystem.token
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+object WeskiColor {
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+
+    val Gray100 = Color(0xFF070707)
+    val Gray90 = Color(0xFF171D23)
+    val Gray80 = Color(0xFF222931)
+    val Gray70 = Color(0xFF303A45)
+    val Gray60 = Color(0xFF515E6F)
+    val Gray50 = Color(0xFF878E9B)
+    val Gray40 = Color(0xFFC6C8CE)
+    val Gray30 = Color(0xFFEAEBEE)
+    val Gray20 = Color(0xFFF7F8F9)
+    val Gray10 = Color(0xFFFFFFFF)
+
+    val Main01 = Color(0xFF7280FF)
+    val Main02 = Color(0xFFA2B6FF)
+    val Main03 = Color(0xFFBDD1FB)
+    val Main04 = Color(0xFFD0DFFF)
+    val Main05 = Color(0xFFEDF3FF)
+    val Main06 = Color(0xFFEDF3FF)
+
+    val Sub01 = Color(0xFFFFDE6B)
+    val Sub02 = Color(0xFFFF9928)
+    val Sub04 = Color(0xFF447EFF)
+    val Sub05 = Color(0xFF4D44FF)
+}
