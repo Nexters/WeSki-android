e54ba86310569e30ef8d9c162373dd8938e9e32c
jgeun <pjk8926@gmail.com>
2025-01-19 11:31:42 +0900

    chore: version up (1.4.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 
 	defaultConfig {
 		applicationId = "com.dieski.weski"
-		versionName = "1.3.0"
-		versionCode = 2025_01_07_01
+		versionName = "1.4.0"
+		versionCode = 2025_01_19_01
 	}
 
 	signingConfigs {
